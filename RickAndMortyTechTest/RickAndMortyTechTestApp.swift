ad3fc0d2224a3ebbf253bfa723a64217d3534b9e
JDiego-Garcia <diiegogarcs26@gmail.com>
2025-10-23 12:54:01 -0600

    Merge remote-tracking branch 'origin/main'

---
+++ RickAndMortyTechTest/RickAndMortyTechTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  RickAndMortyTechTestApp.swift
+//  RickAndMortyTechTest
+//
+//  Created by Juan Diego Garcia Serrano on 23/10/25.
+//
+
+import SwiftUI
+
+@main
+struct RickAndMortyTechTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ RickAndMortyTechTestTests/RickAndMortyTechTestTests.swift
@@ -0,0 +1,17 @@
+//
+//  RickAndMortyTechTestTests.swift
+//  RickAndMortyTechTestTests
+//
+//  Created by Juan Diego Garcia Serrano on 23/10/25.
+//
+
+import Testing
+@testable import RickAndMortyTechTest
+
+struct RickAndMortyTechTestTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
